7b0309fb9a357d67327ff5f3ffc859247cc13afc
GabrielcitoHub <82218456+GabrielcitoHub@users.noreply.github.com>
2025-07-10 20:19:24 -0300

    tilesetfight stuff

---
+++ scripts/world/tilesets/tilesetfight.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.11.2" name="tilesetfight" tilewidth="40" tileheight="40" tilecount="36" columns="6" tilerendersize="grid">
+ <editorsettings>
+  <export target="tilesetfight.lua" format="lua"/>
+ </editorsettings>
+ <image source="../../../assets/sprites/tilesets/tilesetfight.png" width="240" height="240"/>
+</tileset>

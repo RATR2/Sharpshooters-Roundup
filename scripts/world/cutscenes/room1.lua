bac601bbfbee8780dd1c62bcb3f1548aab4219e5
GabrielcitoHub <82218456+GabrielcitoHub@users.noreply.github.com>
2025-07-10 20:16:49 -0300

    fixed rooms

--- scripts/world/cutscenes/room1.lua
+++ scripts/world/cutscenes/room1.lua
@@ -1,74 +1,39 @@
 return {
-    -- The inclusion of the below line tells the language server that the first parameter of the cutscene is `WorldCutscene`.
-    -- This allows it to fetch us useful documentation that shows all of the available cutscene functions while writing our cutscenes!
-
     ---@param cutscene WorldCutscene
-    wall = function(cutscene, event)
-        -- Open textbox and wait for completion
-        cutscene:text("* The wall seems cracked.")
-
-        -- If we have Susie, play a cutscene
+    
+    encounter = function(cutscene, event)
         local susie = cutscene:getCharacter("susie")
-        if susie then
-            -- Detach camera and followers (since characters will be moved)
-            cutscene:detachCamera()
-            cutscene:detachFollowers()
-
-            -- All text from now is spoken by Susie
-            cutscene:setSpeaker(susie)
-            cutscene:text("* Hey,[wait:5] think I can break\nthis wall?", "smile")
-
-            -- Get the bottom-center of the broken wall
-            local x = event.x + event.width/2
-            local y = event.y + event.height/2
-
-            -- Move Susie up to the wall over 0.75 seconds
-            cutscene:walkTo(susie, x, y + 40, 0.75, "up")
-            -- Move other party members behind Susie
-            cutscene:walkTo(Game.world.player, x, y + 100, 0.75, "up")
-            if cutscene:getCharacter("ralsei") then
-                cutscene:walkTo("ralsei", x + 60, y + 100, 0.75, "up")
-            end
-            if cutscene:getCharacter("noelle") then
-                cutscene:walkTo("noelle", x - 60, y + 100, 0.75, "up")
-            end
-
-            -- Wait 1.5 seconds
-            cutscene:wait(1.5)
-
-            -- Walk back,
-            cutscene:wait(cutscene:walkTo(susie, x, y + 60, 0.5, "up", true))
-            -- and run forward!
-            cutscene:wait(cutscene:walkTo(susie, x, y + 20, 0.2))
-
-            -- Slam!!
-            Assets.playSound("impact")
-            susie:shake(4)
-            susie:setSprite("shock_up")
-
-            -- Slide back a bit
-            cutscene:slideTo(susie, x, y + 40, 0.1)
-            cutscene:wait(1.5)
-
-            -- owie
-            susie:setAnimation({"away_scratch", 0.25, true})
-            susie:shake(4)
-            Assets.playSound("wing")
-
-            cutscene:wait(1)
-            cutscene:text("* Guess not.", "nervous")
-
-            -- Reset Susie's sprite
-            susie:resetSprite()
-
-            -- Reattach the camera
-            cutscene:attachCamera()
-
-            -- Align the follower positions behind Kris's current position
-            cutscene:alignFollowers()
-            -- And reattach them, making them return to their target positions
-            cutscene:attachFollowers()
-            Game:setFlag("wall_hit", true)
-        end
+        local ralsei = cutscene:getCharacter("ralsei")
+        local cam_x, cam_y = cutscene:getMarker("cam")
+        cutscene:detachCamera()
+        cutscene:detachFollowers()
+        cutscene:panTo(cam_x, cam_y)
+        cutscene:walkTo("kris", "krisloc", 1)
+        cutscene:walkTo("susie", "susieloc", 1)
+        cutscene:wait(cutscene:walkTo("ralsei", "ralseiloc", 1))
+        cutscene:setSpeaker(susie)
+        cutscene:text("* Finally.[wait:2]\n* Took long enough to get in here", "nervous_side")
+        cutscene:setSpeaker(ralsei)
+        cutscene:text("* Odd...[wait:2]\n* All that's inside is...", "smile_side")
+        cutscene:setSpeaker(susie)
+        cutscene:text("* Tenna...?", "surprise")
+        Assets.playSound("impact")
+        cutscene:wait(1)
+        cutscene:setSpeaker()
+        cutscene:text("* YOU MUST LEAVE.")
+        cutscene:setSpeaker(susie)
+        cutscene:text("* What? [wait:1]\n* Tenna come on, lets get you out of here...", "nervous")
+        cutscene:setSpeaker(ralsei)
+        cutscene:text("* Susie... i dont think that's-[next:true]", "pensive")
+        cutscene:setSpeaker()
+        ralsei:setSprite("battle/hurt_1")
+        Assets.playSound("impact")
+        cutscene:text("* NO.[wait:2]\n* IT IS TOO LATE.")
+        ralsei:setSprite("walk/right_1")
+        cutscene:text("* IT FEEDS ON ATTENTION.[wait:1]\n* AND IT HAS BEEN STARVING.")
+        Assets.playSound("hurt")
+        cutscene:text("* FOR A LONG.[wait:10]\n* [sound:hurt]LONG.[wait:10]\n* TIM[sound:hurt][next:true]")
+        Assets.playSound("impact")
+        susie:setSprite("shock_right")
     end
 }

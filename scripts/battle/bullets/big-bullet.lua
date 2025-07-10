cee283901a44400629fae77de7ea9ceb20444250
GabrielcitoHub <82218456+GabrielcitoHub@users.noreply.github.com>
2025-07-10 13:57:46 -0300

    boom retake lol

--- scripts/battle/bullets/big-bullet.lua
+++ scripts/battle/bullets/big-bullet.lua
@@ -7,12 +7,15 @@ function BigBullet:init(x, y, dir, speed)
     -- Move the bullet in dir radians (0 = right, pi = left, clockwise rotation)
     self.physics.direction = dir
     -- Speed the bullet moves (pixels per frame at 30FPS)
+    self.scale_x = 6
+    self.scale_y = 6
     self.physics.speed = speed
 end
 
 function BigBullet:update()
+    self.physics.speed = self.physics.speed + 1
     -- For more complicated bullet behaviours, code here gets called every update
-    if self.x <= 125 then
+    if self.x <= Game.battle.arena.left then
         self:explode()
     end
     super.update(self)
--- scripts/battle/waves/cowshooter.lua
+++ scripts/battle/waves/cowshooter.lua
@@ -1,15 +1,17 @@
 local Basic, super = Class(Wave)
 
 function Basic:onStart()
+    self.time = 12
     -- Every 0.33 seconds...
     self.timer:every(1/2, function()
         -- Our X position is offscreen, to the right
-        local x = SCREEN_WIDTH + 20
+        local x = SCREEN_WIDTH + 80
         -- Get a random Y position between the top and the bottom of the arena
-        local y = Utils.random(Game.battle.arena.top, Game.battle.arena.bottom)
+        local spawnLocs = {135,205}
+        local y = spawnLocs[love.math.random( #spawnLocs )]
 
         -- Spawn smallbullet going left with speed 8 (see scripts/battle/bullets/smallbullet.lua)
-        local bullet = self:spawnBullet("big-bullet", x, y, math.rad(180), 22)
+        local bullet = self:spawnBullet("big-bullet", x, y, math.rad(180), 11)
 
         -- Dont remove the bullet offscreen, because we spawn it offscreen
         bullet.remove_offscreen = false

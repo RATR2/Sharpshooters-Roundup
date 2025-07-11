debb6e2f49e8adff5b79c4bb01d6547fc485aba9
R4T ᵈᵉᵛ <potatobear000@gmail.com>
2025-07-10 23:29:01 -0500

    cactus.

--- scripts/world/cutscenes/room1.lua
+++ scripts/world/cutscenes/room1.lua
@@ -72,12 +72,14 @@ return {
     fakecactus = function(cutscene, event)
         local cactusinspects = Game:getFlag("cactusinspects") or 0
         if cactusinspects == 0 then
+            cutscene:text("* A cactus.")
+        elseif cactusinspects == 1 then
             cutscene:text("* Something feels weird about this one...")
             cutscene:text("* You just don't quite know what is it...")
-        elseif cactusinspects == 1 then
+        elseif cactusinspects == 2 then
             cutscene:text("* ...?")
             cutscene:text("* This is actually a cardboard\nreplica of a cactus...[wait:1]\n* What is this?!")
-        elseif cactusinspects >= 2 then
+        elseif cactusinspects >= 3 then
             cutscene:text("* A cardboard replica of a cactus.")
         end
     end,

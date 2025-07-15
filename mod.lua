58ebcd9a646a48572938d8aab2e9c0ec33409f6e
GabrielcitoHub <82218456+GabrielcitoHub@users.noreply.github.com>
2025-07-15 02:27:40 -0300

    added code for tenna voice

--- mod.lua
+++ mod.lua
@@ -1,3 +1,21 @@
 function Mod:init()
     print("Loaded "..self.info.name.."!")
 end
+
+function Mod:onTextSound(sound, node)
+    if sound == "tenna" then
+        Assets.playSound(Utils.pick{
+            "voice/tenna1",
+            "voice/tenna2",
+            "voice/tenna3",
+            "voice/tenna4",
+            "voice/tenna5",
+            "voice/tenna6",
+            "voice/tenna7",
+            "voice/tenna8",
+            "voice/tenna9",
+            "voice/tenna10"
+        })
+        self.voice_timer = 6
+    end
+end

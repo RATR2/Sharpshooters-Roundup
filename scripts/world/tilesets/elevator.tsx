94b0e2a5f5da3987910519f5c13c0ff3983958d7
GabrielcitoHub <82218456+GabrielcitoHub@users.noreply.github.com>
2025-07-17 22:15:19 -0300

    elevator tilemap data

---
+++ scripts/world/tilesets/elevator.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.11.2" name="elevator" tilewidth="40" tileheight="40" tilecount="4" columns="2" tilerendersize="grid">
+ <image source="../../../assets/sprites/tilesets/elevator.png" width="80" height="80"/>
+</tileset>

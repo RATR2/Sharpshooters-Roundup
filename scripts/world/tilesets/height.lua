a71b1a7f5ca719709012d439561c1bc49da50007
GabrielcitoHub <82218456+GabrielcitoHub@users.noreply.github.com>
2025-07-17 22:13:55 -0300

    height tilemap data

---
+++ scripts/world/tilesets/height.lua
@@ -0,0 +1,31 @@
+return {
+  version = "1.11",
+  luaversion = "5.1",
+  tiledversion = "1.11.2",
+  name = "height",
+  class = "",
+  tilewidth = 40,
+  tileheight = 40,
+  spacing = 0,
+  margin = 0,
+  columns = 2,
+  image = "../../../assets/sprites/tilesets/height.png",
+  imagewidth = 80,
+  imageheight = 40,
+  objectalignment = "unspecified",
+  tilerendersize = "grid",
+  fillmode = "stretch",
+  tileoffset = {
+    x = 0,
+    y = 0
+  },
+  grid = {
+    orientation = "orthogonal",
+    width = 40,
+    height = 40
+  },
+  properties = {},
+  wangsets = {},
+  tilecount = 2,
+  tiles = {}
+}
---
+++ scripts/world/tilesets/height.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.11.2" name="height" tilewidth="40" tileheight="40" tilecount="2" columns="2" tilerendersize="grid">
+ <image source="../../../assets/sprites/tilesets/height.png" width="80" height="40"/>
+</tileset>
